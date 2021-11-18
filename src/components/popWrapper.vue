490fc3f4f9dc952b26dd434b644c4979efa464c5
Sandra <a0960529965@gmail.com>
2021-11-18 17:59:47 +0800

    pop style

--- src/components/popWrapper.vue
+++ src/components/popWrapper.vue
@@ -1,7 +1,24 @@
 <template>
-        <div>
-        <button @click="showPopup">查看<i class='bx bx-show'></i></button>
-        <van-popup v-model="show" round position="bottom" :style="{ height: '30%' }" />
+    <div>
+        <button @click="showPopup">顯示列表<i class='bx bx-show'></i></button>
+        <van-popup v-model="show" round position="bottom" :style="{ height: '30%' }">
+            <div class="card">
+                <ul>
+                    <li>捷運美麗島站(10號出口)</li>
+                    <li><i class='bx bxs-map'></i>中山一路168號前方</li>
+                    <li>
+                        <div class="rent-bike">
+                            <span>5</span>
+                            <span>可租借</span>
+                        </div>
+                        <div class="return-bike">
+                            <span>8</span>
+                            <span>可歸還</span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </van-popup>
     </div>
 </template>
 
@@ -21,6 +38,17 @@ showPopup() {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.card{
+    border: 1px #aaa solid ;
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px;
+    li:first-child{
+        border-left: 3px solid #FFB82E;
+        line-height: 27px;
+        padding-left: 15px;
+    }
+}
 </style>
